473c24a4a7ec902211381ac0ec0e0a2b117aac08
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-08-05 01:46:28 +0300

    Create CR image

--- pkg/handlers/imageController.go
+++ pkg/handlers/imageController.go
@@ -1,26 +1,19 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"html/template"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-func (h *Handler) imageUpload(ctx *gin.Context){
-	file, err := ctx.FormFile("imageFile")
+func (h *Handler) imageUpload(c *gin.Context){
+	file, err := c.FormFile("imageFile")
 	if err != nil {
 		logrus.Errorf("error on controller upload, %s", err)
 	}
 
-	fmt.Printf("Uploaded file: %s", file.Filename)
-	fmt.Printf("Size of uploaded file: %d", file.Size)
-	fmt.Printf("Header of uploaded file: %s", file.Header)
-
-	tempFile, err := ioutil.TempFile("templates\\img", "upload-*.png")
+	tempFile, err := ioutil.TempFile("D:\\image-softcery\\templates\\img", "upload-*.png")
 	if err != nil{
 		logrus.Errorf("Error during creating temp image, %s", err)
 	}
@@ -34,31 +27,21 @@ func (h *Handler) imageUpload(ctx *gin.Context){
 
 	tempFile.Write(fileBytes)
 
-	h.service.Image.Upload(tempFile.Name())
-	ctx.String(http.StatusOK, "Successfully Uploaded File\n")
-}
-
-func (h *Handler) imageDownload(ctx *gin.Context){
-
-}
-
-func (h *Handler) uploadTemplate(ctx *gin.Context){
-	ctx.HTML(http.StatusOK, "upload.html", gin.H{
-		"title":    "Upload image",
-	})
-}
-
-func (h *Handler) downloadTemplate(ctx *gin.Context){
-	id := ctx.Param("id")
-	image, err := h.service.Download(id)
-	if err != nil{
-		logrus.Errorf("Error on download template, %s", err)
+	_ , err = h.service.Image.Upload(tempFile.Name())
+	if err != nil {
+		logrus.Errorf("Error on uploading image, %s", err)
 	}
-	testString := strings.TrimPrefix(image.Path, "templates\\")
-	ctx.HTML(http.StatusOK, "download.html", gin.H{
-		"Path": template.URL(testString),
-	})
+
+	c.String(http.StatusOK, "Successfully Uploaded File\n")
 }
 
+func (h *Handler) imageDownload(c *gin.Context){
+	id := c.Param("id")
 
-
+	image, err := h.service.Image.Download(id)
+	if err != nil {
+		logrus.Errorf("Error during downloading image... %s", err)
+	}
+	
+    c.File(image.Path)
+}
